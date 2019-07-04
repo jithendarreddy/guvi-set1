4cb52b429786c4008d78dcf683fd6fb9a1731c08
jithendarreddy <pj8142055073@gmail.com>
2019-07-04 14:51:43 +0530

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,9 @@
+d,e=map(int,input().split())
+for k in range(d,e):
+    if k>1:
+        for i in range(2,k):
+            if k%i==0:
+                break
+        else:
+            print(k,end=" ")
+        
