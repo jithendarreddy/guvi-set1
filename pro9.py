f35836777da15816eb7d16c62b2b53e6ea23e036
jithendarreddy <pj8142055073@gmail.com>
2019-07-07 21:29:45 +0530

    Add files via upload

--- pro9.py
+++ pro9.py
@@ -1,5 +1,12 @@
-N=int(input())
-k=list(map(int,input().split()))
-for i in range(len(k)-1):
-    if k[i]+k[i+1]==0 or k[i]+k[i+1]==1 or k[i]+k[i+1]==-1:
-        print(k[i],end=" ")
+A=int(input(""))
+n2=list(map(int,input().split()))
+p=len(n2)
+large=max(n2)
+u,v=0,0
+for i in range(0,p-1):
+ for j in range(i+1,p):
+  if abs(n2[i]+n2[j])< large:
+   u,v=n2[i],n2[j]
+   large=abs(u+v)
+print(u, v)
+    
