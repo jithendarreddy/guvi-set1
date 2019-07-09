36daf8a1eb898510bc46ec40f40d175c5dc90bfd
jithendarreddy <pj8142055073@gmail.com>
2019-07-09 17:55:21 +0530

    Update pro10.py

--- pro10.py
+++ pro10.py
@@ -1,7 +1,7 @@
 N,M=map(int,input().split())
 A=list(map(int,input().split()))
 B=list(map(int,input().split()))
-if set(A).issubset(set(B)):
+if set(B).issubset(set(A)):
     print("YES")
 else:
     print("NO")
