034e62dfa8231932df6b8604660b5d7fb7dbd980
jithendarreddy <pj8142055073@gmail.com>
2019-07-10 09:35:35 +0530

    Update pro15.py

--- pro15.py
+++ pro15.py
@@ -1,1 +1,11 @@
-
+k=int(input())
+p=list(map(int, input().split()))[:k]
+m=max(p)
+nl=[]
+for i in range(0,len(p)):
+    new=p[i:]
+    f=max(new)
+    if(p[i]==f):
+        nl.apend(p[i])
+print(*nl)
+print(m)
