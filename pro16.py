f417b03e444cd8e87b187751cfbea75c4ac4057f
jithendarreddy <pj8142055073@gmail.com>
2019-07-10 17:53:48 +0530

    Update pro16.py

--- pro16.py
+++ pro16.py
@@ -1,7 +1,8 @@
 n,k=map(int,input().split())
 l=list(map(int,input().split()))
-l.sort()
-a=l.index(k)
-l.pop(a)
-print(l[0],l[1],l[2])
+a=[[abs(i1-k),i1]for i1 in l]
+a.sort()
+a=a[1:]
+a=[i1[1] for i1 in a[:3]]
+print(*a)
               
