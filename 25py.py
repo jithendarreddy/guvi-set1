f09927436495c356ad5cc4fb10e4cf18913ea637
jithendarreddy <pj8142055073@gmail.com>
2019-07-21 19:53:05 +0530

    Add files via upload

---
+++ 25py.py
@@ -0,0 +1,14 @@
+a=input()
+l=list(map(int,input().split()))
+maxi=0
+i=0
+while(i<len(l)-1):
+    co=0
+    while(i<len(l)-1 and l[i]<l[i+1]):
+        co+=1
+        i+=1
+    if(co>maxi):
+        maxi=co
+    i+=1
+print(maxi+1)
+
