121da3247584b16c5c48dc7846be6ec16bc18d82
jithendarreddy <pj8142055073@gmail.com>
2019-07-21 20:26:31 +0530

    Update 26py.py

--- 26py.py
+++ 26py.py
@@ -1,14 +1,7 @@
-a=input()
-l=list(map(int,input().split()))
-maxi=0
-i=0
-while(i<len(l)-1):
-    co=0
-    while(i<len(l)-1 and (l[i]<l[i+1] or l[i]==l[i+1])):
-        co+=1
-        i+=1
-    if(co>maxi):
-        maxi=co
-    i+=1
-print(maxi+1)
-
+p=int(input())
+q=list(map(int,input().split()))
+a=1
+for i in range(0,len(q)-1):
+    if q[i]<q[i+1]:
+        a+=1
+print(a)
