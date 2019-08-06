04c51df35fbc0d2cf77a39da3b69d494bcf90dbb
jithendarreddy <pj8142055073@gmail.com>
2019-08-07 01:13:11 +0530

    Add files via upload

--- beg35.py
+++ beg35.py
@@ -1,6 +1,11 @@
-k=input()
-p=0
-for i in k:
-    if i.isnumeric():
-        p+=1
-print(p)
+string = input()
+alphabets = digits = special = 0
+
+for i in range(len(string)):
+    if(string[i].isalpha()):
+        alphabets = alphabets + 1
+    elif(string[i].isdigit()):
+        digits = digits + 1
+    else:
+        special = special + 1
+print(special)
